f89526d16c7e82b3d2f630c02e8999f3027e15f7
Rsaluja02 <raushansaluja@gmail.com>
2024-07-27 22:43:04 +0530

    final commit

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import { SubHeading } from "../../components"
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='newsLetter'>
+    <div className='newLetter-heading'>
+      <SubHeading title='Newsletter'/>
+      <h1 className='headtext__cormorant'>Subscribe to our Newsletter!</h1>
+      <p className='p__opensans'>And never miss latest Updates!</p>
+    </div>
+    <div className='newsLetter-input flex__center'>
+      <input type='email' placeholder='Enter your email address!'/>
+      <button className='custom__button'>Subscribe!</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='footer section__padding'>
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className='footer-links'>
+      <div className='footer-links-contact'>
+        <h1 className='footer-headtext'>Contact Us</h1>
+        <p className='p__opensans'>9 W 53rd St, New York, NY 10019, USA</p>
+        <p className='p__opensans'>+1 212-344-1230</p>
+        <p className='p__opensans'>+1 212-555-1230</p>
+      </div>
+      <div className='footer-links-logo'>
+        <img src={images.reserve} />
+        <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others.”</p>
+        <img src={images.spoon} alt='spoon' className='spoon__img' style={{ marginTop: 15 }} />
+        <div className='footer-links-icons'>
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className='footer-links-work'>
+        <h1 className='footer-headtext'>Working Hours</h1>
+        <p className='p__opensans'>Monday-Friday:</p>
+        <p className='p__opensans'>08:00am - 12:00am</p>
+        <p className='p__opensans'>Saturday-Sunday:</p>
+        <p className='p__opensans'>07:00am - 11:00pm</p>
+      </div>
+    </div>
+
+    <div className='footer-copyright'>
+      <p className='p__opensans'>2024 The Reserve. All Rights Reserved.</p>
+    </div>
   </div>
 );
 
